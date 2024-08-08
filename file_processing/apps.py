58166c9aa1aa2a17d676c8d08f083d1584856d2f
Konstantin Touev <konstantin.touev@gmail.com>
2024-08-08 17:22:06 +0200

    Cleanup temporary folder on exit

--- file_processing/apps.py
+++ file_processing/apps.py
@@ -1,9 +1,11 @@
+import atexit
 import os
 
 from django.apps import AppConfig
 
 from file_processing.document_processor.colbert_utils import test_colbert, initialise_search_component
 from file_processing.file_queue_management.file_queue_db import create_sqlite_database
+from file_processing.storage_manager import delete_temp_dir
 
 
 class FileProcessingConfig(AppConfig):
@@ -15,4 +17,11 @@ class FileProcessingConfig(AppConfig):
             # This check ensure we run only once
             create_sqlite_database()
             initialise_search_component()
+
             # test_colbert()
+            def on_exit():
+                print('Server is stopping...')
+                # Perform your cleanup actions here
+                delete_temp_dir()
+
+            atexit.register(on_exit)
